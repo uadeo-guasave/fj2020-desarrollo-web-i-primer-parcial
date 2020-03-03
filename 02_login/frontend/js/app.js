3015a295a0ad24b7dca2b552144f09e6a8d0829e
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2020-03-03 10:19:45 -0700

    Agregar jQuery al proyecto
    Se agregó jquery
    Se probó el código para el botón submit con js

---
+++ 02_login/frontend/js/app.js
@@ -0,0 +1,18 @@
+/**
+ * Javascript para conectar a la API
+ * 
+ * Se requiere hacer peticiones AJAX a la API
+ * http://localhost:8000/login.php
+ * y obtendrá como resultado un JSON.
+ * 
+ * AJAX: Asyncronic Javascript And XML
+ */
+
+function validarUsuario() {}
+
+$(document).ready(function () {
+    $('#btnLogin').click(function (e) {
+        e.preventDefault();
+        alert("si hace algo");
+    });
+});
